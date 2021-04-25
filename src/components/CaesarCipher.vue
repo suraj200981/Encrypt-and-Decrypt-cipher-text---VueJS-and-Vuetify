f7cb377dc8b60f45b2c477170e8906ff22a10bb0
ss2261 <ss2261@kent.ac.uk>
2021-04-25 15:34:34 +0100

    ceasar component and view created

---
+++ src/components/CaesarCipher.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+  helslo
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'CaesarCipher',
+
+    data: () => ({
+     
+    }),
+  }
+</script>
--- src/views/CaesarView.vue
+++ src/views/CaesarView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CaesarCipher />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CaesarCipher from '@/components/CaesarCipher.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    CaesarCipher
   }
 }
 </script>
