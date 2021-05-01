9a0f416ed79206b58b8ec6462af257c7894303f1
ss2261 <ss2261@kent.ac.uk>
2021-05-01 17:07:12 +0100

    caesar cipher encrpytion complete

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,48 @@
 <template>
   <v-app>
     <v-card class="overflow-hidden">
-    <v-app-bar
-      absolute
-      color="#6A76AB"
-      dark
-      shrink-on-scroll
-      prominent
-      fade-img-on-scroll
-    >
-      <template v-slot:img="{ props }">
-        <v-img
-          v-bind="props"
-          gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
-        ></v-img>
-      </template>
-
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Cipher Encryption Decryption App</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-menu
-        bottom
-        left
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            icon
-            color="yellow"
-            v-bind="attrs"
-            v-on="on"
-          >
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
+      <v-app-bar absolute color="#6A76AB" dark prominent>
+        <template v-slot:img="{ props }">
+          <v-img
+            v-bind="props"
+            gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+          ></v-img>
         </template>
-      </v-menu>
 
-      <template v-slot:extension>
-        <v-tabs align-with-title>
-          <v-tab to="/">Caesar</v-tab>
-          <v-tab to="/about">Vigenere</v-tab>
-          <v-tab>Transposition</v-tab>
-        </v-tabs>
-      </template>
-    </v-app-bar>
-    <v-sheet
-      id="scrolling-techniques-4"
-      max-height="600"
-    >
-      <v-container style="height: 225px;"></v-container>
-    </v-sheet>
-  </v-card>
-   
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Cipher Encryption Decryption App</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-menu bottom left>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon color="yellow" v-bind="attrs" v-on="on">
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+        </v-menu>
+
+        <template v-slot:extension>
+          <v-tabs align-with-title>
+            <v-tab to="/">Caesar</v-tab>
+            <v-tab to="/about">Vigenere</v-tab>
+            <v-tab>Transposition</v-tab>
+          </v-tabs>
+        </template>
+      </v-app-bar>
+      <v-sheet>
+        <v-container style="height: 176px"></v-container>
+      </v-sheet>
+    </v-card>
 
     <!-- Sizes your content based upon application components -->
     <v-main>
@@ -72,55 +53,47 @@
       </v-container>
     </v-main>
 
-    <v-footer
-    dark
-    padless
-  >
-    <v-card
-      flat
-      tile
-      class="indigo lighten-1 white--text text-center"
-    >
-      <v-card-text>
-        <v-btn
-          v-for="icon in icons"
-          :key="icon"
-          class="mx-4 white--text"
-          icon
-        >
-          <v-icon size="24px">
-            {{ icon }}
-          </v-icon>
-        </v-btn>
-      </v-card-text>
+    <v-footer dark padless>
+      <v-card flat tile class="indigo lighten-1 white--text text-center">
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text"
+            icon
+          >
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
 
-      <v-card-text class="white--text pt-0">
-        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-      </v-card-text>
+        <v-card-text class="white--text pt-0" style="width=100px">
+          This app is still being developed and is not yet completed. Please
+          feel free to make changes where possible. This app is still being
+          developed and is not yet completed. Please feel free to make changes
+          where possible.This app is still being developed and is not yet
+          completed. Please feel free to make changes where possible. This app
+          is still being developed and is not yet completed. Please feel free to
+          make changes where possible.
+        </v-card-text>
 
-      <v-divider></v-divider>
+        <v-divider></v-divider>
 
-      <v-card-text class="white--text">
-        {{ new Date().getFullYear() }} — <strong>Suraj Sharma</strong>
-      </v-card-text>
-    </v-card>
-  </v-footer>
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Suraj Sharma</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 <script>
 export default {
-  name: 'App',
+  name: "App",
   data: () => ({
-      icons: [
-        'mdi-facebook',
-        'mdi-twitter',
-        'mdi-linkedin',
-        'mdi-instagram',
-      ],
-    }),
-  }
-
-
+    icons: ["mdi-github", "mdi-linkedin"],
+  }),
+};
 </script>
 
 <style lang="scss">
--- src/components/CaesarCipher.vue
+++ src/components/CaesarCipher.vue
@@ -1,28 +1,71 @@
 <template>
   <v-container>
-    <v-textarea
-      outlined
-      name="input-7-4"
-      label="Plain text"
-      value=""
-      v-model="plainText"
-    ></v-textarea>
+    <v-row>
+      <v-textarea
+        outlined
+        name="input-7-4"
+        label="Plain text"
+        v-model="plainText"
+      ></v-textarea>
+      <v-col class="d-flex" cols="12" sm="6">
+        <v-btn @click="encryptCipher()">Encrypt</v-btn>
+      </v-col>
+    </v-row>
     <br />
     <br />
     <v-textarea
       background-color="amber lighten-4"
       color="orange orange-darken-4"
-      :value="plainText"
+      :value="newString"
     ></v-textarea>
   </v-container>
 </template>
-
+<style scoped>
+</style>
 <script>
 export default {
   name: "CaesarCipher",
 
   data: () => ({
-    plainText: ""
+    plainText: "",
+    newString: "",
+    encryptKey: 3,
   }),
+
+  methods: {
+    encryptCipher() {
+  
+  // Make an output variable
+  var output = "";
+
+  // Go through each character
+  for (var i = 0; i < this.plainText.length; i++) {
+    // Get the character we'll be appending
+    var c = this.plainText[i];
+
+    // check letter with regular expression
+    if (c.match(/[a-z]/i)) {
+      // Get its char code ascii
+      var code = this.plainText.charCodeAt(i);
+
+      // Uppercase letters
+      if (code >= 65 && code <= 90) {
+        c = String.fromCharCode(((code - 65 + this.encryptKey) % 26) + 65);
+      }
+
+      // Lowercase letters
+      else if (code >= 97 && code <= 122) {
+        c = String.fromCharCode(((code - 97 + this.encryptKey) % 26) + 97);
+      }
+    }
+
+    // Append
+    output += c;
+  }
+
+  // All done!
+  this.newString= output;
+    },
+  },
 };
 </script>
